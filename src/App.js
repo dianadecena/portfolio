c10cf448650c137d81a431bfda9d93eb28f92748
Diana Decena <41495265+dianadecena@users.noreply.github.com>
2019-12-19 12:28:45 -0400

    mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import bg from './neon-lights.png';
+import bgmobile from './neon-mobile.png';
 
 const App = () => {
 
   return (
       <div className="container">
         <img src={bg} className="bg" alt="bg"/>
+        <img src={bgmobile} className="bg-mobile" alt="bg-mobile"/>
         <div className="about">
           <span>about me</span>
         </div>
