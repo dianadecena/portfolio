df17966c5d6cb7e328ca9d759976014c164e864a
Diana Decena <41495265+dianadecena@users.noreply.github.com>
2019-12-19 10:57:25 -0400

    push inicial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import bg from './neon-lights.png';
+
+const App = () => {
+
+  return (
+      <div className="container">
+        <img src={bg} className="bg" alt="bg"/>
+        <div className="about">
+          <span>about me</span>
+        </div>
+        <div className="projects">
+          <span>projects</span>
+        </div>
+        <div className="contact">
+          <span>contact</span>
+        </div>
+        <div className="box-info">
+          <div className="nombre">
+            <span>DIANA DECENA</span>
+          </div>
+          <div className="descripcion">
+            <p>I'M CURRENTLY STUDYING SYSTEMS ENGINEERING.</p>
+            <p>CARACAS, VENEZUELA.</p>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default App;
