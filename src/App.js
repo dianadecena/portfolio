87d0c1fdbc122e2ad07ea4ba2704fdc1ecc2b4fc
Diana Decena <41495265+dianadecena@users.noreply.github.com>
2019-12-20 18:16:16 -0400

    contact-box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import bg from './neon-lights.svg';
 import bgmobile from './neon-mobile.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram, faLinkedin} from "@fortawesome/free-brands-svg-icons"
 
 const App = () => {
 
@@ -9,15 +12,6 @@ const App = () => {
       <div className="container">
         <img src={bg} className="bg" alt="bg"/>
         <img src={bgmobile} className="bg-mobile" alt="bg-mobile"/>
-        <div className="about">
-          <span>about me</span>
-        </div>
-        <div className="projects">
-          <span>projects</span>
-        </div>
-        <div className="contact">
-          <span>contact</span>
-        </div>
         <div className="box-info">
           <div className="nombre">
             <span>DIANA DECENA</span>
@@ -26,6 +20,12 @@ const App = () => {
             <p>I'M CURRENTLY STUDYING SYSTEMS ENGINEERING.</p>
             <p>CARACAS, VENEZUELA.</p>
           </div>
+        </div>
+        <div className="contact-box">
+          <a href="https://www.github.com/dianadecena" className="github-icon"><FontAwesomeIcon icon={faGithub} color="white" size="3x"/></a>
+          <a href="https://www.instagram.com/dianadecena" className="instagram-icon"><FontAwesomeIcon icon={faInstagram} color="white" size="3x"/></a>
+          <a href="https://www.linkedin.com/in/diana-decena-1a9b9215a" className="linkedin-icon"><FontAwesomeIcon icon={faLinkedin} color="white" size="3x"/></a>
+          <a href="mailto:dianadecena78@gmail.com" className="email-icon"><FontAwesomeIcon icon={faEnvelope} color="white" size="3x"/></a>
         </div>
     </div>
   );
